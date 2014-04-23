95c2cc77732ef5806e80bd5d68efe2d2a771936d
Josh Fowler & Brentwood Reid <student@epicodus.com>
2014-04-23 09:15:31 -0700

    Add index and create to Nonprofit

--- app/controllers/nonprofits_controller.rb
+++ app/controllers/nonprofits_controller.rb
@@ -2,5 +2,18 @@ class NonprofitsController < ApplicationController
 
   def index
     @nonprofits = Nonprofit.all
+    @nonprofit= Nonprofit.new
   end
+
+  def create
+    @nonprofit = Nonprofit.create(strong)
+    redirect_to root_url
+  end
+end
+
+
+private
+
+def strong
+  params.require(:nonprofit).permit(:name)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 NonProfit::Application.routes.draw do
   resources :nonprofits
+  root to: 'nonprofits#index'
 end
